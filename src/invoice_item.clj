5305a63ea906c8fe6320b29f478a0c25742a47ec
MagusMachinae <Edward.Hughes1911@gmail.com>
2024-04-15 14:12:37 +0100

    implement filtering predicates

--- src/invoice_item.clj
+++ src/invoice_item.clj
@@ -10,3 +10,16 @@
     :or                {discount-rate 0}}]
   (* precise-price precise-quantity (discount-factor item)))
 
+(defn valid-item? [item]
+  (not (and (some? (:retentionable/retentions item))
+            (some? (:taxable/taxes item)))))
+
+(defn search-predicate [{retentions :retentionable/retentions
+                         taxes :taxable/taxes}]
+  (or (when retentions
+        (some (fn [{:retention/keys [category rate]}]
+                (and (= category :ret_fuente) (= rate 1))) retentions))
+      (when taxes
+        (some (fn [{:tax/keys [category rate]}]
+                (and (= category :iva) (= rate 19))) taxes))))
+
